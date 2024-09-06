6fed1f22e0368dfd9c727acc9a182fc07d520b95
iaifrns <nsinifranc@gmail.com>
2024-09-06 12:18:44 +0100

    changes in the app

--- src/helper/filter-col.js
+++ src/helper/filter-col.js
@@ -4,8 +4,11 @@ const occurance = (str, char) => str.split(char).length - 1;
 
 export function mergeColumns(csvData, colList) {
   const rows = csvData.split("\n");
-  const colSortList = colList.sort().map(num => parseInt(num-1));
+  let r= colList.map(num => parseInt(num-1))
+  console.log(r.sort((a,b) => a-b))
+  const colSortList = r.sort((a,b) => a-b);
   const spliter = occurance(rows[0], ";") > 2 ? ";" : ",";
+  
   const mergedRows = rows.map((row) => {
     const columns = row.split(spliter);
     let colContent = columns[colSortList[0]];
@@ -18,7 +21,6 @@ export function mergeColumns(csvData, colList) {
     columns[colSortList[0]] = colContent;
     // Remove the last column (assuming it's not needed)
     const newCol = deleteColumnsInFile(columns, colSortList)
-
     return newCol.join(";");
   });
 
